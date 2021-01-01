cd854265a7e07465e6fded07b0d6793e98cde084
APPLE\Radhi <radhi.mighri@polytechnicien.tn>
2021-01-01 12:05:42 +0100

    Search-by-category-static-overview - Step 1 : Define Routes - Step 2 : Configure Router based on our routes - step 3 : Define the Router Outlet - step 4 : Set up Router Links to pass category id param - step 5 : Enhance ProductListComponent to read category id param - step 6 : Update Angular Service to call new URL on Spring Boot app

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,20 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
 import { Product } from 'src/app/common/product';
+import { ActivatedRoute } from '@angular/router';
 @Component({
 selector: 'app-product-list',
-//templateUrl: './product-list.component.html',
 templateUrl: './product-list-grid.component.html',
 styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+
 products: Product[];
-constructor(private productService: ProductService) { }
+currentCategoryId: number;
+
+constructor(private productService: ProductService,
+            private route: ActivatedRoute) { }
+
 ngOnInit() {
+this.route.paramMap.subscribe(() => {
 this.listProducts();
+});  
 }
+
 listProducts() {
-this.productService.getProductList().subscribe(
+    
+    //check if "id" parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+        // get the "id" parm string, convert it to a number using the "+" symbol
+        this.currentCategoryId = +this.route.snapshot.paramMap.get('id')
+    } else {
+        // no category id available... default to category id 1
+        this.currentCategoryId = 1
+    }
+
+    // now get the products for the given category id
+this.productService.getProductList(this.currentCategoryId).subscribe(
 data => {
 this.products = data;
 }
